c1d03d10bb477caaa2e56077069b8cdd45d12fcb
RubenOdegard <ruben.odegard@gmail.com>
2024-03-13 21:56:45 +0100

    Updated spotify route

--- src/app/api/spotify/route.ts
+++ src/app/api/spotify/route.ts
@@ -4,55 +4,50 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 
 async function refreshToken() {
-  try {
-    const tokenResponse = await axios.get("/api/token");
-    const accessToken = tokenResponse.data?.accessToken;
-    return accessToken;
-  } catch (error) {
-    console.error("Error refreshing token:", error);
-    throw new Error("Failed to refresh access token");
-  }
+	try {
+		const tokenResponse = await axios.get("/api/token");
+		const accessToken = tokenResponse.data?.accessToken;
+		return accessToken;
+	} catch (error) {
+		console.error("Error refreshing token:", error);
+		throw new Error("Failed to refresh access token");
+	}
 }
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const url = searchParams.get("url");
+	const { searchParams } = new URL(request.url);
+	const url = searchParams.get("url");
 
-  try {
-    axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+	try {
+		axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
-    // Get a new access token
-    let accessToken = null;
-    try {
-      accessToken = await refreshToken();
-    } catch (refreshError) {
-      console.error("Error refreshing token:", refreshError);
-      return NextResponse.json({ error: AxiosError }, {
-        status: 401,
-      });
-    }
+		let accessToken = null;
+		try {
+			accessToken = await refreshToken();
+		} catch (refreshError) {
+			console.error("Error refreshing token:", refreshError);
+			return NextResponse.json(
+				{ error: AxiosError },
+				{
+					status: 401,
+				},
+			);
+		}
 
-    // Debug
-    console.log("Access Token:", accessToken);
+		const artistResponse = await axios.get(url!, {
+			headers: {
+				Authorization: `Bearer ${accessToken}`,
+			},
+		});
 
-    // Retry the original request with the new token
-    const artistResponse = await axios.get(
-      url!,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      },
-    );
+		const artistData = artistResponse.data;
 
-    const artistData = artistResponse.data;
-
-    return NextResponse.json({ artistData });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({
-      errorMessage: "Error in Spotify Route",
-      error: error,
-    });
-  }
+		return NextResponse.json({ artistData });
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json({
+			errorMessage: "Error in Spotify Route",
+			error: error,
+		});
+	}
 }
