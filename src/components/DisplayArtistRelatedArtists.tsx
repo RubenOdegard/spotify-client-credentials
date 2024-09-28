369244a9073b58d7d3782055bbdbc9145c3f781f
RubenOdegard <ruben.odegard@gmail.com>
2024-09-28 13:52:35 +0200

    UI Changes

--- src/components/DisplayArtistRelatedArtists.tsx
+++ src/components/DisplayArtistRelatedArtists.tsx
@@ -11,77 +11,73 @@ import Image from "next/image";
 import { Artist } from "@/types/SpotifyArtistRelatedArtists";
 
 const DisplayArtistRelatedArtist = ({ artistID }: { artistID: string }) => {
-  const { artistID: contextArtistID, handleRelatedArtistClick } = useArtist();
+	const { artistID: contextArtistID, handleRelatedArtistClick } = useArtist();
 
-  const dynamicUrl =
-    `https://api.spotify.com/v1/artists/${artistID}/related-artists`;
-  const { artistRelatedArtists, loading, error } =
-    useSpotifyArtistRelatedArtists(dynamicUrl);
+	const dynamicUrl = `https://api.spotify.com/v1/artists/${artistID}/related-artists`;
+	const { artistRelatedArtists, loading, error } =
+		useSpotifyArtistRelatedArtists(dynamicUrl);
 
-  if (loading) {
-    return (
-      <span className="col-span-12 row-span-1 row-start-5 animate-pulse rounded-md bg-emerald-950">
-      </span>
-    );
-  }
+	if (loading) {
+		return (
+			<span className="col-span-12 row-span-1 row-start-5 animate-pulse rounded-md bg-emerald-950"></span>
+		);
+	}
 
-  if (error) {
-    return <p>{error}</p>;
-  }
+	if (error) {
+		return <p>{error}</p>;
+	}
 
-  return (
-    <div className="row-start-8 group col-span-12 row-span-1 w-full">
-      {artistRelatedArtists
-        ? (
-          <>
-            <h2 className="mb-4 flex items-center gap-2 font-semibold">
-              <UsersIcon size={18} className="text-emerald-500" />
-              Related Artists
-            </h2>
-            <div className="relative flex cursor-pointer snap-x snap-proximity flex-row gap-2 overflow-x-scroll pb-2">
-              {artistRelatedArtists.artistData?.artists?.map((
-                artist: Artist,
-              ) => (
-                // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
-                <div
-                  key={artist.id}
-                  className="flex snap-start flex-col items-center justify-center gap-y-1.5 rounded-md border border-emerald-950 bg-emerald-950 p-4"
-                  onClick={() => handleRelatedArtistClick(artist.id)}
-                >
-                  <div className="relative h-24 w-24">
-                    <Image
-                      src={artist.images[0].url}
-                      alt=""
-                      quality={25}
-                      height={artist.images[0].height}
-                      width={artist.images[0].width}
-                      className="aspect-square rounded-full border-2 border-emerald-800/20 shadow-inner duration-500 md:grayscale md:group-hover:grayscale-0"
-                    />
-                    <p className="absolute right-0 top-0 rounded-md border border-emerald-500 bg-emerald-800/90 px-1 py-0.5 text-[0.6em]">
-                      {nFormatter(artist.followers.total, 1)}
-                    </p>
-                  </div>
+	return (
+		<div className="row-start-8 group col-span-12 row-span-1 w-full">
+			{artistRelatedArtists ? (
+				<>
+					<h2 className="mb-4 flex items-center gap-2 font-semibold">
+						<UsersIcon size={18} className="text-emerald-500" />
+						Related Artists
+					</h2>
+					<div className="relative flex cursor-pointer snap-x snap-proximity flex-row gap-2 overflow-x-scroll pb-2">
+						{artistRelatedArtists.artistData?.artists?.map((artist: Artist) => (
+							// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+							<div
+								key={artist.id}
+								className="flex snap-start flex-col items-center justify-center gap-y-1.5 rounded-md border border-emerald-950 bg-emerald-950 p-4"
+								onClick={() => handleRelatedArtistClick(artist.id)}
+							>
+								<div className="relative h-24 w-24">
+									<Image
+										src={artist.images[0].url}
+										alt={artist.name}
+										quality={50}
+										height={artist.images[0].height}
+										width={artist.images[0].width}
+										className="aspect-square rounded-full border-2 border-emerald-800/20 object-cover shadow-inner duration-500 md:grayscale md:group-hover:grayscale-0"
+									/>
+									<p className="absolute right-0 top-0 rounded-md border border-emerald-500 bg-emerald-800/90 px-1 py-0.5 text-[0.6em]">
+										{nFormatter(artist.followers.total, 1)}
+									</p>
+								</div>
 
-                  <p className="max-w-[15ch] truncate text-xs text-emerald-300">
-                    {artist.name}
-                  </p>
-                  <div className="relative mt-0.5 flex w-full items-center justify-center gap-1">
-                    <div className="absolute -left-[1px] top-0 z-40 flex aspect-square h-3 w-3 items-center justify-center rounded-l-full text-red-800">
-                      <FlameIcon
-                        size={12}
-                        className="ml-1 fill-red-500 text-red-400"
-                      />
-                    </div>{" "}
-                    <Progress value={artist.popularity} />
-                  </div>
-                </div>
-              ))}
-            </div>
-          </>
-        )
-        : <p>No artist data available</p>}
-    </div>
-  );
+								<p className="max-w-[15ch] truncate text-xs text-emerald-300">
+									{artist.name}
+								</p>
+								<div className="relative mt-0.5 flex w-full items-center justify-center gap-1">
+									<div className="absolute -left-[1px] top-0 z-40 flex aspect-square h-3 w-3 items-center justify-center rounded-l-full text-red-800">
+										<FlameIcon
+											size={12}
+											className="ml-1 fill-red-500 text-red-400"
+										/>
+									</div>{" "}
+									<Progress value={artist.popularity} />
+								</div>
+							</div>
+						))}
+					</div>
+				</>
+			) : (
+				<p>No artist data available</p>
+			)}
+		</div>
+	);
 };
 
 export default DisplayArtistRelatedArtist;
--- src/components/ModuleDisplayContent.tsx
+++ src/components/ModuleDisplayContent.tsx
@@ -7,19 +7,19 @@ import DisplayArtistTopTracks from "./DisplayArtistTopTracks";
 import { useArtist } from "./ArtistContext";
 
 const ModuleDisplayContent = () => {
-  const { artistID } = useArtist();
-  return (
-    <div className="lg:p-24">
-      <div className="mx-auto grid min-h-[100dvh] grid-cols-12 gap-x-8 gap-y-6 border-emerald-900 bg-background/30 p-4 shadow-xl backdrop-blur-sm md:mx-2 md:min-h-[80dvh] md:rounded-xl md:border lg:mx-auto lg:p-8">
-        <div className="col-span-12 flex w-full items-center justify-center lg:py-8">
-          <DisplayArtist artistID={artistID} />
-        </div>
-        <DisplayArtistTopTracks artistID={artistID} />
-        <DisplayArtistAlbums artistID={artistID} />
-        <DisplayArtistRelatedArtists artistID={artistID} />
-      </div>
-    </div>
-  );
+	const { artistID } = useArtist();
+	return (
+		<div className="lg:p-24">
+			<div className="mx-auto grid min-h-[100dvh] grid-cols-12 gap-x-8 gap-y-6 border-emerald-900 bg-background/30 p-4 shadow-xl backdrop-blur-sm md:mx-2 md:min-h-[80dvh] md:rounded-xl md:border lg:mx-auto lg:p-8">
+				<div className="col-span-12 flex w-full items-center justify-center lg:py-8">
+					<DisplayArtist artistID={artistID} />
+				</div>
+				<DisplayArtistTopTracks artistID={artistID} />
+				<DisplayArtistAlbums artistID={artistID} />
+				<DisplayArtistRelatedArtists artistID={artistID} />
+			</div>
+		</div>
+	);
 };
 
 export default ModuleDisplayContent;
