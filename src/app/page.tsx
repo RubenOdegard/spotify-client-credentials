92fa066f15250d539f44ae07067eae59f38008ca
RubenOdegard <ruben.odegard@gmail.com>
2024-09-28 13:52:21 +0200

    Changed container css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import ModuleDisplayContent from "@/components/ModuleDisplayContent";
 
 const Home = () => {
-  return (
-    <div className="md:px-4 xl:px-24 flex justify-center items-center mx-auto min-h-[100dvh]">
-      <ModuleDisplayContent />
-    </div>
-  );
+	return (
+		<div className="mx-auto flex min-h-[100dvh] items-center justify-center md:px-4 xl:px-24">
+			<ModuleDisplayContent />
+		</div>
+	);
 };
 
 export default Home;
